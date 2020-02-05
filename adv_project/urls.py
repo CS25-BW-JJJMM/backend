a724edf75af1d32033d2941e97936754813c4f86
Mike Harley <mharley12345@gmail.com>
2020-02-04 17:31:00 -0800

    .

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from adventure.api import RoomViewSet,initialize
 from adventure.urls import *
 from api.urls import *
-from django.views.generic import TemplateView
+
 #from api.views import ChatView
 from rest_framework.authtoken import views
 from graphene_django.views import GraphQLView
@@ -20,9 +20,6 @@ urlpatterns = [
     path('api/adv/', include('adventure.urls')),
     re_path(r'^api-token-auth/', views.obtain_auth_token),
     path('graphql/', GraphQLView.as_view(graphiql=True)),
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger_ui.html',
-        extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
+
     
 ]
