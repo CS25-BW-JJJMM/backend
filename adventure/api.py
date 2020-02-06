2d28f7f2ad8bdc7107d577631fe716547dd51106
Jonathan Dorety <kyullog@gmail.com>
2020-02-05 19:51:11 -0800

    Fix ReferenceError, move no longer crashes

--- adventure/api.py
+++ adventure/api.py
@@ -67,7 +67,7 @@ def move(request):
         return JsonResponse({'name': player.user.username, 'title': nextRoom.title, 'description': nextRoom.description, 'room_id': nextRoom.id, 'players': players, 'error_msg': ""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'room_id': nextRoom.id, 'players': players, 'error_msg': "You cannot move that way."}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'room_id': room.id, 'players': players, 'error_msg': "You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
